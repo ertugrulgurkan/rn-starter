931489c3e2acc11377a64108ae4b4f08970c3cfc
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-15 21:38:14 +0300

    exersize new comp.

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import Exersize from "./src/screens/ExersiceComponent"
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    Exersize: Exersize
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "Exersize",
     defaultNavigationOptions: {
       title: "My App"
     }
