413ea9ee41b23a086ebcf0628f38b4666b1f4463
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-11-15 23:45:40 +0300

    navigate components in home screen

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const navigator = createStackNavigator(
     List: ListScreen
   },
   {
-    initialRouteName: "List",
+    initialRouteName: "Home",
     defaultNavigationOptions: {
       title: "My App"
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+const HomeScreen = ({navigation}) => {
+  //console.log(props.navigation);
+  return (
+    <View>
+      <Text style={styles.text}>
+        Hi there!
+      </Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to Components Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate('Exersize')}
+        title="Go to Exersise Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
